9c1bff47b5f1d4b654c059987e2080c6319626b1
Jeeva4000 <vjeeva@gmail.com>
2023-07-14 20:11:31 +0530

    intial commit

--- Authentication/auth.js
+++ Authentication/auth.js
@@ -1,14 +1,19 @@
-import Jwt  from "jsonwebtoken";
+import Jwt from "jsonwebtoken";
 
-
-export async function isAuthenticated(req,res,next){
-    const token = req.headers["x-auth-token"];
-    console.log("token---",token);
-    if(!token){
-        console.log("data")
-        return res.status(400).json({data:"Invalid Authorization"})
+export async function isAuthenticated(req, res, next) {
+  const token = req.headers["x-auth-token"];
+  console.log("token---", token);
+  if (!token) {
+    console.log("data");
+    return res.status(400).json({ data: "Invalid Authorization" });
+  }
+  Jwt.verify(token, process.env.SECRETKEY, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ error: "Invalid token" });
     }
-    Jwt.verify(token, process.env.SECERTKEY)
-    // console.log("validuser--",validUser)
+    req.user = decoded;
     next();
-} 
+  });
+}
+
+
