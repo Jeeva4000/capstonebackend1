323126d53c0dde90c412b2d61af88d085dc42cd4
Jeeva4000 <vjeeva@gmail.com>
2023-10-08 20:04:46 +0530

    intial commit

--- Controllers/user.js
+++ Controllers/user.js
@@ -1,37 +1,14 @@
-// import { client } from "../db.js";
-// import  Jwt  from "jsonwebtoken";
-
-
-// export function addUser(userInfo){
-//     return client
-//     .db("cars")
-//     .collection("users")
-//     .insertOne(userInfo)
-// }
-
-// export function getUser(userEmail){
-//     return client
-//     .db("cars")
-//     .collection("users")
-//     .findOne({email:userEmail})
-// }
-
-// export function generateJwtToken(id){
-//     return Jwt.sign({id},
-//         process.env.SECRETKEY,
-//         {expiresIn:"30d"})
-// }  
 
 // user.js
 
 import { client } from "../db.js";
 import Jwt from "jsonwebtoken";
 
-export  function addUser(userInfo) {
+export function addUser(userInfo) {
   return client.db("cars").collection("users").insertOne(userInfo);
 }
 
-export  function getUser(userEmail) {
+export function getUser(userEmail) {
   return client.db("cars").collection("users").findOne({ email: userEmail });
 }
 
